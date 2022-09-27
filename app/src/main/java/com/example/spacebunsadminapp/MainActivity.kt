443d2c85d80399154b135b65ab72a10151c45625
Annebel Choong <annebelchoong@gmail.com>
2022-09-27 16:57:14 +0800

    Add drawer

--- app/src/main/java/com/example/spacebunsadminapp/MainActivity.kt
+++ app/src/main/java/com/example/spacebunsadminapp/MainActivity.kt
@@ -3,19 +3,21 @@ package com.example.spacebunsadminapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.*
 import com.example.spacebunsadminapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private val nav by lazy { supportFragmentManager.findFragmentById(R.id.host)!!.findNavController() }
     private lateinit var abc: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         abc = AppBarConfiguration(
             setOf(
@@ -24,17 +26,21 @@ class MainActivity : AppCompatActivity() {
                 R.id.order,
                 R.id.product,
             ),
-            binding.root
+            binding.drawerLayout
         )
 
-        binding.navView.setupWithNavController(abc)
+        setupActionBarWithNavController(nav, abc) //control the action bar. Add abc it can swap the hamburger menu
+        binding.navView.setupWithNavController(nav)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menu?.setGroupDividerEnabled(true)
-        menuInflater.inflate(R.menu.drawer, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menu?.setGroupDividerEnabled(true)
+//        menuInflater.inflate(R.menu.drawer, menu)
+//        return super.onCreateOptionsMenu(menu)
+//    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return nav.navigateUp(abc) || super.onSupportNavigateUp()
+    }
 
 }
