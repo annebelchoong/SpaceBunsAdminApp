3c774cf68088f665795a659a885c34ae3e3bc925
Annebel Choong <annebelchoong@gmail.com>
2022-09-27 16:18:01 +0800

    Add nav bar drawer and menu and add code to MainActivity.kt

--- app/src/main/java/com/example/spacebunsadminapp/MainActivity.kt
+++ app/src/main/java/com/example/spacebunsadminapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.spacebunsadminapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.ui.*
+import com.example.spacebunsadminapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var abc: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        abc = AppBarConfiguration(
+            setOf(
+                R.id.dashboard,
+                R.id.user,
+                R.id.order,
+                R.id.product,
+            ),
+            binding.root
+        )
+
+        binding.navView.setupWithNavController(abc)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.setGroupDividerEnabled(true)
+        menuInflater.inflate(R.menu.drawer, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
