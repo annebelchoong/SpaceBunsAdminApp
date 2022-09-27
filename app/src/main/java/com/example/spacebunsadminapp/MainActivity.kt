83f8fc3971fb9001ac6e4fbef1aae57c9d47e8c0
Annebel Choong <annebelchoong@gmail.com>
2022-09-27 20:29:22 +0800

    remove unwanted code

--- app/src/main/java/com/example/spacebunsadminapp/MainActivity.kt
+++ app/src/main/java/com/example/spacebunsadminapp/MainActivity.kt
@@ -33,12 +33,6 @@ class MainActivity : AppCompatActivity() {
         binding.navView.setupWithNavController(nav)
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menu?.setGroupDividerEnabled(true)
-//        menuInflater.inflate(R.menu.drawer, menu)
-//        return super.onCreateOptionsMenu(menu)
-//    }
-
     override fun onSupportNavigateUp(): Boolean {
         return nav.navigateUp(abc) || super.onSupportNavigateUp()
     }
