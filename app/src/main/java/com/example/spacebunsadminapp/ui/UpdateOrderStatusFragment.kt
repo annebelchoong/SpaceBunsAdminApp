14f538af8158b6b830f73f522c1c794a3a8908fc
Annebel Choong <annebelchoong@gmail.com>
2022-09-30 20:26:24 +0800

    Change UpdateOrderStatusFragment.kt into a dialog fragment

--- app/src/main/java/com/example/spacebunsadminapp/ui/UpdateOrderStatusFragment.kt
+++ app/src/main/java/com/example/spacebunsadminapp/ui/UpdateOrderStatusFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
 import com.example.spacebunsadminapp.R
 import com.example.spacebunsadminapp.databinding.FragmentUpdateOrderStatusBinding
 
-class UpdateOrderStatusFragment : Fragment() {
+class UpdateOrderStatusFragment : DialogFragment() {
 
     private lateinit var binding: FragmentUpdateOrderStatusBinding
     private val nav by lazy { findNavController() }
