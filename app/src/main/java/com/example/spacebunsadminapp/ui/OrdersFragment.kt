85d0ebd3f23c6eb14111f7d37a753cd4e40af952
Annebel Choong <annebelchoong@gmail.com>
2022-10-01 18:56:37 +0800

    Link adapter and viewmodel

--- app/src/main/java/com/example/spacebunsadminapp/ui/OrdersFragment.kt
+++ app/src/main/java/com/example/spacebunsadminapp/ui/OrdersFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.spacebunsadminapp.R
 import com.example.spacebunsadminapp.data.OrderViewModel
 import com.example.spacebunsadminapp.databinding.FragmentOrdersBinding
 import com.example.spacebunsadminapp.util.OrderAdapter
+import kotlinx.coroutines.launch
 
 class OrdersFragment : Fragment() {
 
@@ -22,8 +26,22 @@ class OrdersFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentOrdersBinding.inflate(inflater, container, false)
-        return binding.root
 
+
+        adapter = OrderAdapter {holder, order ->
+            holder.binding.root.setOnClickListener{
+                nav.navigate(R.id.orderDetailsFragment, bundleOf("id" to order.orderId))
+            }
+        }
+
+        binding.rvOrders.adapter = adapter
+        binding.rvOrders.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+
+        vm.getAll().observe(viewLifecycleOwner){
+            adapter.submitList(it)
+            binding.txtCount.text = "${it.size} Order(s)"
+        }
+        return binding.root
     }
 
 }
