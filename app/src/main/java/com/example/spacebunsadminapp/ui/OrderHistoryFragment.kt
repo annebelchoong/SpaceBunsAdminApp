76202415225f4ca9541aa8cc2a224cbb3abaafc1
Annebel Choong <annebelchoong@gmail.com>
2022-10-01 23:59:34 +0800

    Add new order and order history fragment for tab

---
+++ app/src/main/java/com/example/spacebunsadminapp/ui/OrderHistoryFragment.kt
@@ -0,0 +1,21 @@
+package com.example.spacebunsadminapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.spacebunsadminapp.databinding.FragmentOrderHistoryBinding
+
+
+class OrderHistoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentOrderHistoryBinding
+    private val nav by lazy { findNavController() }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentOrderHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
