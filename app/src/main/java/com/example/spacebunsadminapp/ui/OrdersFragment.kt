289b21f38df6ae66b3747a9d7ba60a664990662d
Annebel Choong <annebelchoong@gmail.com>
2022-10-01 17:41:20 +0800

    Add viewmodel and adapter

--- app/src/main/java/com/example/spacebunsadminapp/ui/OrdersFragment.kt
+++ app/src/main/java/com/example/spacebunsadminapp/ui/OrdersFragment.kt
@@ -5,18 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.spacebunsadminapp.R
+import com.example.spacebunsadminapp.data.OrderViewModel
 import com.example.spacebunsadminapp.databinding.FragmentOrdersBinding
+import com.example.spacebunsadminapp.util.OrderAdapter
 
 class OrdersFragment : Fragment() {
 
     private lateinit var binding: FragmentOrdersBinding
     private val nav by lazy { findNavController() }
+    private val vm:OrderViewModel by activityViewModels()
+
+    private lateinit var  adapter: OrderAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentOrdersBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
 }
