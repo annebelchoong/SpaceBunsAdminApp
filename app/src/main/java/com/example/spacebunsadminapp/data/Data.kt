c74e5561f79f988a04174c18e460b3b3cf28ef98
Annebel Choong <annebelchoong@gmail.com>
2022-10-01 17:40:46 +0800

    Added the data

--- app/src/main/java/com/example/spacebunsadminapp/data/Data.kt
+++ app/src/main/java/com/example/spacebunsadminapp/data/Data.kt
@@ -1,5 +1,38 @@
 package com.example.spacebunsadminapp.data
 
-data class Data(
-    var id: String = ""
-)
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.time.LocalDateTime
+import java.util.*
+
+data class Orders(
+    @DocumentId
+    var orderId: String = "",
+    var dateTime: LocalDateTime,
+    var address: String = "",
+    var orderStatus: String = "",
+    var paymentMethod: String = "",
+    var totalPrice: Double = 0.00,
+    var discount: Double = 0.00,
+
+) {
+    @get:Exclude
+    var count : Int = 0
+}
+
+data class OrderDetails(
+    @DocumentId
+    var id: String = "",
+    var orderId: String = "",
+    var productId: String = "",
+    var productName: String = "",
+    var quantity: String = "",
+    var price: String = "",
+){
+    @get:Exclude
+    var totalPrice: Double = 0.00
+}
+
+val ORDERS = Firebase.firestore.collection("orders")
