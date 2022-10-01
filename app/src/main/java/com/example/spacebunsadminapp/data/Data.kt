19873ede6635e4b2d9c78420357213c2bff4897a
boonyongyang <boonyongyang@gmail.com>
2022-10-02 01:45:37 +0800

    implement vouchers CR, haven't UD

--- app/src/main/java/com/example/spacebunsadminapp/data/Data.kt
+++ app/src/main/java/com/example/spacebunsadminapp/data/Data.kt
@@ -1,5 +1,20 @@
 package com.example.spacebunsadminapp.data
 
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 data class Data(
     var id: String = ""
 )
+
+data class Voucher(
+    @DocumentId
+    var voucherId: String = "",
+    var voucherCode: String = "",
+    var discountPercentage: Double = 0.00,
+    var usedCount: Int = 0,
+)
+
+
+val VOUCHERS = Firebase.firestore.collection("vouchers")
