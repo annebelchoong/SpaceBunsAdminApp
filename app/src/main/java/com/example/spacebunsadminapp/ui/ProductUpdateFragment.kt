bb574998936ccf45b53f6883617b770b621f9be8
Guna <gunaseelann-wg17@student.tarc.edu.my>
2022-10-01 14:57:04 +0800

    Added layouts and few pages

---
+++ app/src/main/java/com/example/spacebunsadminapp/ui/ProductUpdateFragment.kt
@@ -0,0 +1,4 @@
+package com.example.spacebunsadminapp.ui
+
+class ProductUpdateFragment {
+}
--- app/src/main/java/com/example/spacebunsadminapp/ui/ProductsFragment.kt
+++ app/src/main/java/com/example/spacebunsadminapp/ui/ProductsFragment.kt
@@ -8,14 +8,22 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.spacebunsadminapp.R
 import com.example.spacebunsadminapp.databinding.FragmentProductsBinding
+import com.example.spacebunsadminapp.databinding.FragmentProductsListBinding
+import com.example.spacebunsadminapp.databinding.FragmentProductsInsertBinding
+import com.example.spacebunsadminapp.databinding.FragmentProductsUpdateBinding
+import com.example.spacebunsadminapp.databinding.FragmentProductItemBinding
 
 class ProductsFragment : Fragment() {
 
     private lateinit var binding: FragmentProductsBinding
     private val nav by lazy { findNavController() }
+    private val col = "Products"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentProductsBinding.inflate(inflater, container, false)
+
+
+
         return binding.root
     }
 }
