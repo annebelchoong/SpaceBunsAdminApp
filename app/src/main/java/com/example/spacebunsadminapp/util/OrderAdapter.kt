cd1bb1f238abe928a5228214cf49b589f2d193f8
Annebel Choong <annebelchoong@gmail.com>
2022-10-01 17:40:22 +0800

    Link recycle view with adapter

---
+++ app/src/main/java/com/example/spacebunsadminapp/util/OrderAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.spacebunsadminapp.util
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spacebunsadminapp.data.Orders
+import com.example.spacebunsadminapp.databinding.ItemOrderBinding
+
+
+class OrderAdapter (
+    val fn: (ViewHolder, Orders) -> Unit ={ _, _ ->}
+    ): ListAdapter<Orders, OrderAdapter.ViewHolder>(DiffCallBack) {
+
+    companion object DiffCallBack:DiffUtil.ItemCallback<Orders>(){
+        override fun areItemsTheSame(a: Orders, b: Orders) = a.orderId == b.orderId
+        override fun areContentsTheSame(a: Orders, b: Orders) = a == b
+    }
+    class ViewHolder(val binding: ItemOrderBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemOrderBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val order = getItem(position)
+
+        holder.binding.txtId.text = order.orderId
+        holder.binding.txtTotal.text = order.totalPrice.toString()
+
+//        holder.binding.txtCount.text = "${order.count} Order(s)"
+
+        fn(holder,order)
+    }
+
+    }
