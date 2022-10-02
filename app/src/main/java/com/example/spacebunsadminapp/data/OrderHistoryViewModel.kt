8aafbc7ce2216c8d263aa25bf16e5d2ef2e2369c
Annebel Choong <annebelchoong@gmail.com>
2022-10-02 10:44:55 +0800

    Put code in order history from orders

--- app/src/main/java/com/example/spacebunsadminapp/data/OrderHistoryViewModel.kt
+++ app/src/main/java/com/example/spacebunsadminapp/data/OrderHistoryViewModel.kt
@@ -6,7 +6,7 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObjects
 import com.google.firebase.ktx.Firebase
 
-class OrderViewModel: ViewModel() {
+class OrderHistoryViewModel: ViewModel() {
 
     private val orders = MutableLiveData<List<Orders>>()
 
--- app/src/main/java/com/example/spacebunsadminapp/ui/OrderHistoryFragment.kt
+++ app/src/main/java/com/example/spacebunsadminapp/ui/OrderHistoryFragment.kt
@@ -5,17 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.example.spacebunsadminapp.R
+import com.example.spacebunsadminapp.data.OrderHistoryViewModel
 import com.example.spacebunsadminapp.databinding.FragmentOrderHistoryBinding
+import com.example.spacebunsadminapp.util.OrderAdapter
 
 
 class OrderHistoryFragment : Fragment() {
 
     private lateinit var binding: FragmentOrderHistoryBinding
     private val nav by lazy { findNavController() }
+    private val vm: OrderHistoryViewModel by activityViewModels()
+
+    private lateinit var  adapter: OrderAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentOrderHistoryBinding.inflate(inflater, container, false)
+
+        binding.btnInsert.setOnClickListener { nav.navigate(R.id.insertOrderFragment) }
+
+        adapter = OrderAdapter {holder, order ->
+            holder.binding.root.setOnClickListener{
+                nav.navigate(R.id.orderDetailsFragment, bundleOf("id" to order.orderId))
+            }
+        }
+
+        binding.rvOrders.adapter = adapter
+        binding.rvOrders.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+
+        vm.getAll().observe(viewLifecycleOwner){
+            adapter.submitList(it)
+            binding.txtCount.text = "${it.size} order(s)"
+        }
         return binding.root
     }
 }
