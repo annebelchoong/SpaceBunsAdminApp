79da89894138e1111800c3dd74dbe34fbe8a95c0
Annebel Choong <annebelchoong@gmail.com>
2022-10-02 15:32:52 +0800

    Add vm for new orders

---
+++ app/src/main/java/com/example/spacebunsadminapp/data/NewOrdersViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.spacebunsadminapp.data
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObjects
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+
+class NewOrdersViewModel: ViewModel() {
+
+    private val orders = MutableLiveData<List<Orders>>()
+
+    private val col = Firebase.firestore.collection("orders")
+
+    init {
+        col.addSnapshotListener { value,_ -> orders.value = value?.toObjects() }
+    }
+
+    fun init() = Unit
+
+    fun get(id:String) = orders.value?.find { it.orderId == id }
+
+    fun getAll() = orders
+
+    fun getAllProcessingOrders() {
+        orders.value?.forEach { it.orderStatus == "Processing" }
+    }
+
+//    suspend fun getAllProcessingOrders(): List<Orders> {
+//        return ORDERS.get().await().toObjects<Orders>()
+//    }
+
+    fun delete(id: String){
+        col.document(id).delete()
+    }
+
+    fun deleteAll(){
+        orders.value?.forEach { col.document(it.orderId).delete() }
+    }
+
+    fun set(o:Orders){
+        col.document(o.orderId).set(o)
+    }
+}
