9768b3022ec610f1134f540f35a7cd33381acf5e
Annebel Choong <annebelchoong@gmail.com>
2022-10-02 10:45:32 +0800

    change order view model to order history view model

--- app/src/main/java/com/example/spacebunsadminapp/ui/InsertOrderFragment.kt
+++ app/src/main/java/com/example/spacebunsadminapp/ui/InsertOrderFragment.kt
@@ -5,21 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.text.isDigitsOnly
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.example.spacebunsadminapp.R
-import com.example.spacebunsadminapp.data.OrderViewModel
+import com.example.spacebunsadminapp.data.OrderHistoryViewModel
 import com.example.spacebunsadminapp.data.Orders
 import com.example.spacebunsadminapp.databinding.FragmentInsertOrderBinding
-import java.util.*
 
 
 class InsertOrderFragment : Fragment() {
 
     private lateinit var binding: FragmentInsertOrderBinding
     private val nav by lazy { findNavController() }
-    private val vm: OrderViewModel by activityViewModels()
+    private val vm: OrderHistoryViewModel by activityViewModels()
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
