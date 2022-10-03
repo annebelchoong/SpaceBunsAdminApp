a34c7f0c1fd1d0f49e9443ee26ef7b2523f380d7
Annebel Choong <annebelchoong@gmail.com>
2022-10-03 14:14:33 +0800

    Pass data from orderStatus detail and display

--- app/src/main/java/com/example/spacebunsadminapp/ui/OrderDetailsFragment.kt
+++ app/src/main/java/com/example/spacebunsadminapp/ui/OrderDetailsFragment.kt
@@ -6,33 +6,42 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.example.spacebunsadminapp.data.CompleteOrdersViewModel
 import com.example.spacebunsadminapp.data.Orders
+import com.example.spacebunsadminapp.data.OrdersViewModel
 import com.example.spacebunsadminapp.databinding.FragmentOrderDetailsBinding
 import com.example.spacebunsadminapp.util.OrderDetailsAdapter
+import kotlinx.coroutines.launch
 import java.text.DecimalFormat
 
 class OrderDetailsFragment:Fragment() {
     private lateinit var binding: FragmentOrderDetailsBinding
     private val nav by lazy { findNavController() }
-    private val vm: CompleteOrdersViewModel by activityViewModels()
+    private val vm:OrdersViewModel by activityViewModels()
 
     private lateinit var adapter: OrderDetailsAdapter // for products
-    private val id by lazy {arguments?.getString("id","")?: ""}
+    private val id by lazy {arguments?.getString("orderId","")?: ""}
     private val formatter = DecimalFormat("0.00")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentOrderDetailsBinding.inflate(inflater, container, false)
 
+        lifecycleScope.launch {
+            val orders = vm.get(id)!!
+//        if (orders != null) {
+            binding.edtOrderId.text = "${orders.orderId}"
+            binding.edtDate.text = "${orders?.date}"
+            binding.edtAddress.text = "${orders?.address}"
+            binding.edtPaymentMethod.text = "${orders?.paymentMethod}"
+            binding.edtTotal.text = "RM ${orders?.totalPrice}"
+//        }
 
-        val orders = vm.get(id)!!
+        }
 
-        binding.edtOrderId.text = orders.orderId
-        binding.edtDate.text = orders.date.toString()
-        binding.edtAddress.text = orders.address
-        binding.edtPaymentMethod.text = orders.paymentMethod
-        binding.edtTotal.text = "RM " + formatter.format(orders.totalPrice)
+//        val orders = vm.get(id)!!
+//        }
+
 //        when(orders.orderStatus){
 //            "Processing" -> binding.spnNewStatus.selectedItemPosition == 0
 //            "Delivered" -> binding.spnNewStatus.selectedItemPosition == 1
@@ -45,13 +54,18 @@ class OrderDetailsFragment:Fragment() {
     }
 
     private fun update() {
-        val o = Orders(
-//            orderStatus = binding.spnNewStatus.selectedItem as String,
-            orderId = binding.edtOrderId.text.toString().trim(),
-            totalPrice = binding.edtTotal.text.toString().toDoubleOrNull() ?: 0.0,
-            address = binding.edtAddress.text.toString().trim(),
-            paymentMethod = binding.edtPaymentMethod.text.toString().trim()
-        )
+        val o = Orders()
+
+        when(binding.spnNewStatus.selectedItem as String){
+            "Processing" -> o.orderStatusId = "P"
+            "Delivered" -> o.orderStatusId = "D"
+            "Cancelled" -> o.orderStatusId = "C"
+        }
+
+        o.orderId = binding.edtOrderId.text.toString().trim()
+        o.totalPrice = binding.edtTotal.text.toString().toDoubleOrNull() ?: 0.0
+        o.address = binding.edtAddress.text.toString().trim()
+        o.paymentMethod = binding.edtPaymentMethod.text.toString().trim()
 
         vm.set(o)
 
