3cdc3220580d606b74750b8b5a026359c8de35bb
Guna <gunaseelann-wg17@student.tarc.edu.my>
2022-10-04 22:17:59 +0800

    Can view the updated price

--- app/src/main/java/com/example/spacebunsadminapp/util/ProductAdapter.kt
+++ app/src/main/java/com/example/spacebunsadminapp/util/ProductAdapter.kt
@@ -30,6 +30,7 @@ class ProductAdapter (
 
         holder.binding.txtId.text   = product.id
         holder.binding.txtCat.text = ("--> " + product.cat + " <--")
+        holder.binding.txtPrice.text = ("( RM" + product.price + " )")
         holder.binding.txtName.text = product.name
         holder.binding.txtDesc.text  = product.desc
 
