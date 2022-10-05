76e8003897ecd168e56a66cb4e0d31417c58ccfc
chiahuiyun <80665061+chiahuiyun@users.noreply.github.com>
2022-10-05 13:47:18 +0800

    complete staff part

--- app/src/main/java/com/example/spacebunsadminapp/ui/StaffFragment.kt
+++ app/src/main/java/com/example/spacebunsadminapp/ui/StaffFragment.kt
@@ -29,15 +29,15 @@ class StaffFragment : Fragment() {
     ): View {
         binding = FragmentStaffBinding.inflate(inflater, container, false)
 
-        binding.fabtnAddVoucher.setOnClickListener { nav.navigate(R.id.staffInsertFragment) }
+        binding.fabtnAddStaff.setOnClickListener { nav.navigate(R.id.staffInsertFragment) }
 
         adapter = StaffAdapter { holder, staff ->
             holder.binding.root.setOnClickListener {
                 nav.navigate(R.id.userUpdateFragment, bundleOf("staffId" to staff.staffId))
             }
         }
-        binding.rvVouchers.adapter = adapter
-        binding.rvVouchers.addItemDecoration(
+        binding.rvStaffs.adapter = adapter
+        binding.rvStaffs.addItemDecoration(
             DividerItemDecoration(
                 context,
                 DividerItemDecoration.VERTICAL
@@ -49,16 +49,16 @@ class StaffFragment : Fragment() {
         val arrayAdapter =
             ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item)
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spnVoucherAttributes.adapter = arrayAdapter
+        binding.spnStaffAttributes.adapter = arrayAdapter
 
-        val vouchers = vm.getStaffAttributes()
+        val staffs = vm.getStaffAttributes()
         arrayAdapter.add("All")
         arrayAdapter.addAll(staffs)
 
         // TODO: Get all
         vm.getAll().observe(viewLifecycleOwner) {
             adapter.submitList(it)
-            binding.txtCount.text = "${it.size} staff(s)"
+            binding.txtStaffCount.text = "${it.size} staff(s)"
         }
 
         return binding.root
